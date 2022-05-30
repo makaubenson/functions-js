20521dae426098f3a1fe0a8ba5528e637c68430f
Benson <bensonmakau2000@gmail.com>
2022-05-30 14:47:29 +0300

    ES6 way of defining function default params

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # FUNCTIONS
 
 ### Default Parameters
+
+- In ES5 way
+- `const createBooking = function (flightNum, numPassengers, price) { //ES5 numPassengers = numPassengers || 1; price = price || 199; }`
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 //Default Parameters
 const bookings = [];
-const createBooking = function (flightNum, numPassengers, price) {
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
   //ES5
   //   numPassengers = numPassengers || 1;
   //   price = price || 199;
-  
+
   const booking = {
     flightNum,
     numPassengers,
