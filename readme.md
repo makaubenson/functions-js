7eaa9b1b8905781e62d95d8ef53d8740894cb9a8
Benson <bensonmakau2000@gmail.com>
2022-05-30 14:45:54 +0300

    ES5 WAY OF defining default parameters

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # FUNCTIONS
+
+### Default Parameters
--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+//Default Parameters
+const bookings = [];
+const createBooking = function (flightNum, numPassengers, price) {
+  //ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+  
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+console.log(bookings);
